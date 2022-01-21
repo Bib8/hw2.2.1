e7a8aafc9adbae2baac22cc3806034dc79fb3856
pro100 <frostymag177@gmail.com>
2022-01-21 12:23:10 +0300

    Add test obj to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Bicycle bike = new Bicycle("Discovery", 2);
+        bike.updateTyre();
+        Car car = new Car("Ford", 4);
+        car.checkEngine();
+        car.updateTyre();
+        Truck truck = new Truck("GAZ", 8);
+        truck.checkEngine();
+        truck.updateTyre();
+        truck.checkTrailer();
     }
 }
